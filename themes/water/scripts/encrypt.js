6c81cddf2d772b0c68260b74af934075df48a404
suxin2017 <1107178482@qq.com>
2021-07-20 08:26:59 +0800

    fix: data

--- themes/water/scripts/encrypt.js
+++ themes/water/scripts/encrypt.js
@@ -2,10 +2,10 @@ var CryptoJS = require("crypto-js");
 const key = process.env.SECRET_KEY;
 hexo.extend.renderer.register("dmd", "html", function (data, options) {
   return new Promise(function (resolve, reject) {
-    if (key) {
-      var ciphertext = CryptoJS.AES.encrypt(data.text, key).toString();
-      resolve(ciphertext);
-    }
+    // if (key) {
+    //   var ciphertext = CryptoJS.AES.encrypt(data.text, key).toString();
+    //   resolve(ciphertext);
+    // }
     resolve(data.text);
   });
 });
