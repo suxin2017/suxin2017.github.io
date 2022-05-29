81dd170d354e2466668f407e6083a410273b1422
suxin2017 <1107178482@qq.com>
2022-05-29 16:00:45 +0800

    done

---
+++ source/_posts/lua-lexer.md
@@ -0,0 +1,120 @@
+---
+title: lua-lexer
+date: 2022-05-28 19:56:22
+tags:
+category:
+  - post
+---
+
+动手写个词法解析器，词法解析非常简单其关键处理问题在于
+
+- 字符串
+- 数字
+- 关键字与符号
+
+### 定义关键字及符号映射
+
+```javascript preview src=lua-lexer.js
+
+```
+
+```html preview
+<style>
+  .token {
+    display: inline-block;
+    padding: 4px;
+    border: 1px solid var(--base00, transparent);
+    margin: 2px;
+    border-radius: 4px;
+  }
+</style>
+<div>
+  <textarea rows="20" style="width:100%" id="input" autofocus>
+local abc = 123;
+if(abc="123"){
+  print(abc)
+}
+string='123'
+-- abc
+-- [[
+comment
+]]--
+longString = [[too long]]
+</textarea
+  >
+  <button id="generate">generate</button>
+</div>
+<div id="tag" style="width:100%;min-height:200px;font-size:14px;"></div>
+<script>
+  const input = document.getElementById("input");
+  const g = document.getElementById("generate");
+  const t = document.getElementById("tag");
+  const lexerCom = () => {
+    console.log("start..");
+    t.innerHTML = "";
+    let err = "";
+    try {
+      const lexer = new Lexer(input.value, "main.lua");
+      let d = "";
+      let token = lexer.nextToken();
+      while (token[0] !== TOKEN_EOF) {
+        d += gt(token);
+        token = lexer.nextToken();
+      }
+      d += gt(token);
+      t.innerHTML = d;
+    } catch (e) {
+      console.error(e);
+      t.innerHTML = `<div style="color:red;font-size:14px;">${e.message}</div>`;
+    }
+  };
+  g.onclick = lexerCom;
+  lexerCom();
+  function colorRandom() {
+    const color = [
+      "yellow",
+      "orange",
+      "red",
+      "magenta",
+      "violet",
+      "blue",
+      "cyan",
+      "green",
+    ];
+    return color[Math.floor(Math.random() * color.length)];
+  }
+  function gt(token) {
+    let [kind, tok, location] = token;
+    let printStr = `[${kindToCategory(kind)}] ${tok}`;
+
+    return `<div class="token" style="color:var(--${colorRandom()})" data-start=${
+      location?.start
+    } data-end=${location?.end}>${printStr}</div>`;
+  }
+
+  // This handler will be executed every time the cursor
+  // is moved over a different list item
+  t.addEventListener(
+    "mouseover",
+    function (event) {
+      event.target.style.borderColor = "orange";
+      let start = event.target.dataset["start"];
+      let end = event.target.dataset["end"];
+      setTimeout(() => {
+        input.focus();
+        input.setSelectionRange(start, end);
+      }, 10);
+
+      event.preventDefault();
+    },
+    false
+  );
+  t.addEventListener(
+    "mouseout",
+    function (event) {
+      event.target.style.borderColor = "";
+    },
+    false
+  );
+</script>
+```
--- themes/water/scripts/previwe.js
+++ themes/water/scripts/previwe.js
@@ -1,89 +1,108 @@
 var HTML_ESCAPE_TEST_RE = /[&<>"]/;
 var HTML_ESCAPE_REPLACE_RE = /[&<>"]/g;
 var HTML_REPLACEMENTS = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;'
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': "&quot;",
 };
 
 function replaceUnsafeChar(ch) {
-    return HTML_REPLACEMENTS[ch];
+  return HTML_REPLACEMENTS[ch];
 }
 
 function escapeHtml(str) {
-    if (HTML_ESCAPE_TEST_RE.test(str)) {
-        return str.replace(HTML_ESCAPE_REPLACE_RE, replaceUnsafeChar);
-    }
-    return str;
+  if (HTML_ESCAPE_TEST_RE.test(str)) {
+    return str.replace(HTML_ESCAPE_REPLACE_RE, replaceUnsafeChar);
+  }
+  return str;
 }
-const hljs = require('highlight.js')
-const javascript = require('highlight.js/lib/languages/javascript');
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('js', javascript);
-const typescript = require('highlight.js/lib/languages/typescript');
-hljs.registerLanguage('typescript', typescript);
-hljs.registerLanguage('ts', typescript);
-const css = require('highlight.js/lib/languages/css');
-hljs.registerLanguage('css', css);
-const xml = require('highlight.js/lib/languages/xml');
-hljs.registerLanguage('xml', xml);
-hljs.registerLanguage('html', xml);
+const hljs = require("highlight.js");
+const javascript = require("highlight.js/lib/languages/javascript");
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("js", javascript);
+const typescript = require("highlight.js/lib/languages/typescript");
+hljs.registerLanguage("typescript", typescript);
+hljs.registerLanguage("ts", typescript);
+const css = require("highlight.js/lib/languages/css");
+hljs.registerLanguage("css", css);
+const xml = require("highlight.js/lib/languages/xml");
+hljs.registerLanguage("xml", xml);
+hljs.registerLanguage("html", xml);
 
-const url_for = require('hexo-util').url_for.bind(hexo)
-const imgSize = require('markdown-it-imsize');
-hexo.extend.filter.register('markdown-it:renderer', function (md) {
-    const { config } = this; // Optional, parse user 
-    md.use(imgSize)
-    md.use(function (md) {
-        const oldCodeRule = md.renderer.rules.fence;
-        md.renderer.rules.fence = function (tokens, idx, options, env, slf) {
-            const token = tokens[idx];
-            const code = token.content
-            const placeholder = token.info.split(' ')[2] !== 'open' ? token.info.split(' ')[2] : '';
-            const defaultOpen = token.info.split(' ')[2] === 'open';
-            const hasPreview = token.info.indexOf('preview') !== -1;
-            const lang = token.info.split(' ')[0] || '';
-            let codeHighlight = '';
-            if (lang) {
-                try {
-                    codeHighlight = `<pre><code class="hljs">${hljs.highlight(lang, code).value}</code></pre>`
-                } catch (e) {
-                    codeHighlight = `<pre><code class="hljs">${hljs.highlightAuto(code).value}</code></pre>`
-                }
-            } else {
-                codeHighlight = `<pre><code class="hljs">${hljs.highlightAuto(code).value}</code></pre>`
-            }
-            if (!hasPreview) {
-                return codeHighlight;
-            }
-            const jsPreview = token.info.indexOf('js') !== -1 || token.info.indexOf('javascript') !== -1;
-            let demo = '暂不支持该语言预览'
-            if (jsPreview) {
-                demo = `<script type="module">
-    (function(){
-        ${code}
-    })(${idx})
-</script>`
-            }
-            const htmlPreview = token.info.indexOf('html') !== -1;
-            if (htmlPreview) {
-                demo = code
-            }
-            return `<div id="preview-${idx}" class="preview">
+const url_for = require("hexo-util").url_for.bind(hexo);
+const imgSize = require("markdown-it-imsize");
+hexo.extend.filter.register("markdown-it:renderer", function (md) {
+  const { config } = this; // Optional, parse user
+  md.use(imgSize);
+  md.use(function (md) {
+    const oldCodeRule = md.renderer.rules.fence;
+    md.renderer.rules.fence = function (tokens, idx, options, env, slf) {
+      const token = tokens[idx];
+      const code = token.content;
+      const placeholder =
+        token.info.split(" ")[2] !== "open" ? token.info.split(" ")[2] : "";
+      const defaultOpen = token.info.split(" ")[2] === "open";
+      const hasPreview = token.info.indexOf("preview") !== -1;
+      const lang = token.info.split(" ")[0] || "";
+      let codeHighlight = "";
+      if (lang) {
+        try {
+          codeHighlight = `<pre><code class="hljs">${
+            hljs.highlight(lang, code).value
+          }</code></pre>`;
+        } catch (e) {
+          codeHighlight = `<pre><code class="hljs">${
+            hljs.highlightAuto(code).value
+          }</code></pre>`;
+        }
+      } else {
+        codeHighlight = `<pre><code class="hljs">${
+          hljs.highlightAuto(code).value
+        }</code></pre>`;
+      }
+      if (!hasPreview) {
+        return codeHighlight;
+      }
+      const jsPreview =
+        token.info.indexOf("js") !== -1 ||
+        token.info.indexOf("javascript") !== -1;
+      let demo = "暂不支持该语言预览";
+
+      if (jsPreview) {
+        if (token.info.indexOf("src=") !== -1) {
+          demo = `<script src="/js/${token.info.match(/src=(.*)/)?.[1]}">
+${code}</script>`;
+        } else {
+          demo = `<script type="module">
+                    (function(){
+                        ${code}
+                    })(${idx})
+                </script>`;
+        }
+      }
+      const htmlPreview = token.info.indexOf("html") !== -1;
+      if (htmlPreview) {
+        demo = code;
+      }
+      return `<div id="preview-${idx}" class="preview">
 <div class="preview-box">    
-    ${placeholder ? placeholder : ''}${demo}
+    ${placeholder ? placeholder : ""}${demo}
     <div class="preview-box-util">
-        <span id="expand-${idx}" class="preview-box-util-code" style="background-image:url(${url_for('/imgs/code.svg')})"></span>
+        <span id="expand-${idx}" class="preview-box-util-code" style="background-image:url(${url_for(
+        "/imgs/code.svg"
+      )})"></span>
     </div>
 </div>
-<div id="code-${idx}" class="preview-code ${defaultOpen && 'preview-code-active'}">
+<div id="code-${idx}" class="preview-code ${
+        defaultOpen && "preview-code-active"
+      }">
 ${codeHighlight}
 </div>
 <script>
     const expandCode_${idx} = document.getElementById('expand-${idx}');
     const bindCode_${idx} = document.getElementById('code-${idx}');
-    let flag_${idx} = ${defaultOpen ? 'false' : 'true'};
+    let flag_${idx} = ${defaultOpen ? "false" : "true"};
     expandCode_${idx}.onclick = function (){
         if(flag_${idx}){
             bindCode_${idx}.classList.add('preview-code-active')
@@ -94,10 +113,9 @@ ${codeHighlight}
     }
 </script>
 </div>`;
-        }
-    })
+    };
+  });
 });
 
-const backtickCodeBlock = require('hexo/lib/plugins/filter/before_post_render/backtick_code_block.js')
-hexo.extend.filter.unregister('before_post_render', backtickCodeBlock);
-
+const backtickCodeBlock = require("hexo/lib/plugins/filter/before_post_render/backtick_code_block.js");
+hexo.extend.filter.unregister("before_post_render", backtickCodeBlock);
